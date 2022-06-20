ae6284f308cc7194b2e715a9deeb6321dda617ff
canggihwr <canggihrinaldi8@gmail.com>
2022-06-20 19:31:48 +0800

    first-up

---
+++ resources/views/new.blade.php
@@ -0,0 +1,6 @@
+<body bgcolor="black" style="padding: 70px 0;">
+    
+    <b style="text-align: center; "><h1 style="color:white; font-family:arial; letter-spacing: 5px;">HELLO<span style="color:black; background-color: #FFFFFF;"> WORLD.</span></h1></b>
+    <b style="text-align: center;"><h3 style="color:white; font-family:arial;">ハロー・ワールド</h3></b>
+</body>
+
